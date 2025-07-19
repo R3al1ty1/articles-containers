137a074f62ecf775f6cd4af66cf5b73e94106f16
R3al1ty1 <dolfinikkonovalov@gmail.com>
2025-07-19 14:16:09 +0300

    denied access to devtools and extensions

--- chrome_builder/start-chromium.sh
+++ chrome_builder/start-chromium.sh
@@ -15,7 +15,6 @@ exec chromium-browser \
   $EXTENSION_FLAG \
   --disable-sync \
   --disable-background-networking \
-  --homepage "$START_URL" \
-  \
   --disable-quic \
   --proxy-bypass-list="<--localhost-->" \
+  --kiosk "$START_URL"
